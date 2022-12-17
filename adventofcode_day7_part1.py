f3da275d368e422170d02be3afb0f444d5238793
Dakota01011 <dakotakoelling@gmail.com>
2022-12-17 15:15:51 -0600

    Update D7P1
    Still not working

--- adventofcode_day7_part1.py
+++ adventofcode_day7_part1.py
@@ -1,9 +1,9 @@
 # --- Day 7: No Space Left On Device ---
-# PART 1
+# Part 1
 
 from pathlib import Path
 from dataclasses import dataclass, field
-from typing import List
+from typing import List, Union, Any
 
 data = """$ cd /
 $ ls
@@ -29,35 +29,41 @@ $ ls
 5626152 d.ext
 7214296 k"""
 
+dataPath = Path.cwd() / 'adventofcode_day7_input.txt'
+with dataPath.open('r') as f:
+    pass  # data = f.read()
+
 
 @dataclass
 class myDir:
-    dirName: str = ''
-    dirSize: int = 0
-    thingsInDir: List = field(default_factory=list)
+    name: str = ''
+    size: int = 0
+    thingsInIt: List = field(default_factory=list)
+    parent: Union[Any, None] = None
 
-
-inputFilePath = Path.cwd() / 'adventofcode_day7_input.txt'
-with inputFilePath.open('r') as f:
-    pass#data = f.read()
-
-system = myDir('/')
-myPwd = system
-scanningLs = False
+root = myDir(name='/')
+myPwd = root
+parsingLs = False
 for line in data.split('\n'):
+    print(line)
+    parts = line.split(' ')
     if line.startswith('$'):
-        scanningLs = False
-        parts = line.split(' ')
-        if (parts[1] == 'cd') and (myPwd.dirName != parts[2]):
-            newDir = myDir(parts[2])
-            myPwd.thingsInDir
-            myPwd =
-        elif parts[1] == 'ls':
-            scanningLs = True
+        if parts[1] == 'cd':
+            newPath = parts[2]
+            if newPath == '..':
+                myPwd = myPwd.parent
+            elif newPath == myPwd.name:
+                continue
+            else:
+                for d in myPwd.thingsInIt:
+                    if d.name == newPath:
+                        myPwd = d
+        elif parts[1] == "ls":
+            continue
+    elif parts[0] == 'dir':
+        newDir = myDir(name=parts[1], parent=myPwd)
+        myPwd.thingsInIt.append(newDir)
+    else:
+        myPwd.size += int(parts[0])
 
-    if scanningLs:
-
-
-
-
-
+print(root)
